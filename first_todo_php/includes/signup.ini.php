2da4681bc1b5210868f2e1aac1e4eea71a385a49
tothlilla <toth.lillla88@gmail.com>
2019-09-06 09:51:52 +0200

    fix(signup.ini.php): alert works properly

--- first_todo_php/includes/signup.ini.php
+++ first_todo_php/includes/signup.ini.php
@@ -4,12 +4,12 @@
   if(isset($_POST['toDoSubmit'])) {
 	$toDoText = $_POST['toDoText'];
 	if(empty($toDoText)) {
-	  $inputError = "You must fill this";  
-	  echo "<script>alert('$inputError');</script>";
+		$inputError = "You must fill this";  
+	  	echo "<script>alert('".mysqli_real_escape_string($conn, $inputError)."');history.go(-1);</script>";	
 	} else {
-	  $sql = "INSERT INTO userdata (toDoText) VALUE ('$toDoText');";
-	  mysqli_query($conn, $sql);     
-	}
-	header("location: ../index.php");
+		$sql = "INSERT INTO userdata (toDoText) VALUE ('$toDoText');";
+		mysqli_query($conn, $sql);  
+		header("location: ../index.php");
+	}	
   }
 ?>
